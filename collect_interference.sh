655679fb25d215d45bfbeef330d8d6c09ec831c1
Jan Vesely <jan.vesely@rutgers.edu>
2016-10-27 12:22:48 -0400

    collect_interference: Exit and remove fail if the program starts crashing
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

--- collect_interference.sh
+++ collect_interference.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -o pipefail
+
 if [ "x$SUFFIX" == "x" ] ; then
 	SUFFIX=`uname -n`
 fi
@@ -7,6 +9,11 @@ fi
 DIR=data-${SUFFIX}
 
 mkdir -p $DIR
+
+function run()
+{
+	./atomic-interference $1 | grep "Copy BW" | sed 's/[^0-9]*\([0-9\.]*\).*/\1/' | tee -a $2
+}
 
 if [ "x$ITERATIONS" == "x" ] ; then
 	ITERATIONS=20
@@ -16,6 +23,9 @@ for i in 0 16 40 80 160 `seq 320 320 20480` ; do
 	FILE=${DIR}/${i}.data
 	echo -n > $FILE
 	for j in `seq $ITERATIONS` ; do
-		./atomic-interference $i | grep "Copy BW" | sed 's/[^0-9]*\([0-9\.]*\).*/\1/' | tee -a $FILE
+		if ! run $i $FILE; then
+			rm -v $FILE;
+			exit 1;
+		fi
 	done
 done
