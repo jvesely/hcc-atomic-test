8e2a0d687181275ccf4fda3d031a110a5406de24
Jan Vesely <jan.vesely@rutgers.edu>
2017-05-16 17:28:28 -0400

    collect_interference: Log all output to a log file
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

--- collect_interference.sh
+++ collect_interference.sh
@@ -12,7 +12,7 @@ mkdir -p $DIR
 
 function run()
 {
-	./atomic-interference $1 | grep "Copy BW" | sed 's/[^0-9]*\([0-9\.]*\).*/\1/' | tee -a $2
+	./atomic-interference $1 | tee -a $2.log | grep "Copy BW" | sed 's/[^0-9]*\([0-9\.]*\).*/\1/' | tee -a $2
 }
 
 if [ "x$ITERATIONS" == "x" ] ; then
@@ -21,17 +21,18 @@ fi
 
 for i in 0 16 40 80 160 `seq 320 320 20480` ; do
 	FILE=${DIR}/${i}.data
-	rm -f $FILE
+	LOG_FILE=${DIR}/${i}.log
+	rm -f $FILE $LOG_FILE
 	until [ -f $FILE ] ; do
 		for j in `seq $ITERATIONS` ; do
 			if ! run $i $FILE; then
 				GPU_UTIL=`radeontop -d - -l 1 | grep -o 'gpu[^,]*'`
 				if [ "$GPU_UTIL" != "gpu 0.00%" ]; then
 					echo "ERROR: program crashed and the GPU is in not recoverable state. Exiting"
-					rm -v $FILE
+					rm -v $FILE $LOG_FILE
 					exit 1
 				else
-					rm -vf $FILE
+					rm -vf $FILE $LOG_FILE
 					break
 				fi
 			fi
